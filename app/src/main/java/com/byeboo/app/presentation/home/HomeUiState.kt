4095b5cc12a663ea2b405b31d4c321d54fc99d02
fredLeeJH <fred54755475@gmail.com>
2025-08-05 02:08:33 +0900

    #123 [refactor] : 홈 온보딩, 홈 사이드이펙트 및 라우트, 스크린 분리

--- app/src/main/java/com/byeboo/app/presentation/home/HomeUiState.kt
+++ app/src/main/java/com/byeboo/app/presentation/home/HomeUiState.kt
@@ -8,3 +8,7 @@ data class HomeUiState(
     val totalSteps: Int = 30,
     val nickname: String? = "하츠핑"
 )
+sealed interface HomeSideEffect {
+    data object NavigateToQuest : HomeSideEffect
+    data object NavigateToQuestStart : HomeSideEffect
+}
