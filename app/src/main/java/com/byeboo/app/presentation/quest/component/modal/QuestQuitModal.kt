597e71392c4bda06bde4d403b5bbb53d428cf9be
sohee <144779368+sohee6989@users.noreply.github.com>
2025-08-11 05:47:00 +0900

    #131 [refactor] : screenheight로 설정해놓은 부분 그냥 값으로 설정하기

--- app/src/main/java/com/byeboo/app/presentation/quest/component/modal/QuestQuitModal.kt
+++ app/src/main/java/com/byeboo/app/presentation/quest/component/modal/QuestQuitModal.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.byeboo.app.core.designsystem.component.button.ByeBooButton
 import com.byeboo.app.core.designsystem.ui.theme.ByeBooTheme
-import com.byeboo.app.core.util.screenHeightDp
 import com.byeboo.app.core.util.screenWidthDp
 
 @Composable
@@ -41,7 +40,7 @@ fun QuestQuitModal(
                 .fillMaxWidth()
                 .clip(RoundedCornerShape(12.dp))
                 .background(color = ByeBooTheme.colors.gray800)
-                .padding(horizontal = screenWidthDp(24.dp), vertical = screenHeightDp(24.dp)),
+                .padding(horizontal = screenWidthDp(24.dp), vertical = 24.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
@@ -51,7 +50,7 @@ fun QuestQuitModal(
                 color = ByeBooTheme.colors.gray50
             )
 
-            Spacer(modifier = Modifier.height(screenHeightDp(16.dp)))
+            Spacer(modifier = Modifier.height(16.dp))
 
             Text(
                 text = "작성하시던 내용은\n저장되지 않아요.",
@@ -60,7 +59,7 @@ fun QuestQuitModal(
                 textAlign = TextAlign.Center
             )
 
-            Spacer(modifier = Modifier.height(screenHeightDp(16.dp)))
+            Spacer(modifier = Modifier.height(16.dp))
 
             Row(
                 modifier = Modifier.fillMaxWidth(),
