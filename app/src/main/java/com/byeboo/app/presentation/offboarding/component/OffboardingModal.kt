4798171d4cd0468ead18679f0d462be7eb0d0d35
sohee <144779368+sohee6989@users.noreply.github.com>
2025-08-10 23:33:41 +0900

    #133 [refactor] : offboarding 모달 내 여정명 변수로 변경

--- app/src/main/java/com/byeboo/app/presentation/offboarding/component/OffboardingModal.kt
+++ app/src/main/java/com/byeboo/app/presentation/offboarding/component/OffboardingModal.kt
@@ -28,6 +28,7 @@ import com.byeboo.app.core.util.screenWidthDp
 
 @Composable
 fun OffboardingModal(
+    journeyName: String,
     nickname: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
@@ -55,7 +56,7 @@ fun OffboardingModal(
             Spacer(modifier = Modifier.height(screenHeightDp(8.dp)))
 
             Text(
-                text = "감정 직면 여정은\n모두 마무리했어요",
+                text = "{$journeyName}을\n모두 마무리했어요",
                 style = ByeBooTheme.typography.sub3,
                 color = ByeBooTheme.colors.gray50,
                 textAlign = TextAlign.Center
