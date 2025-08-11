73e06d8ea7f93a66c1fb5706c98ad61bc68fa578
sohee <144779368+sohee6989@users.noreply.github.com>
2025-08-12 02:21:55 +0900

    #131 [mod] : MyPageModal shape, color 변경

--- app/src/main/java/com/byeboo/app/presentation/mypage/component/MyPageModal.kt
+++ app/src/main/java/com/byeboo/app/presentation/mypage/component/MyPageModal.kt
@@ -1,5 +1,6 @@
 package com.byeboo.app.presentation.mypage.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,10 +9,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
@@ -37,6 +40,8 @@ fun MyPageModal(
         Column(
             modifier = modifier
                 .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(12.dp))
+                .background(color = ByeBooTheme.colors.gray900Alpha80)
                 .padding(24.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
