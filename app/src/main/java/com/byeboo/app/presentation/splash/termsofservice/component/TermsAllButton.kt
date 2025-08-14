b532b9788a6df0ccfc3219adc05a7c1cc9cc88d4
JAY <wndkdus102@naver.com>
2025-08-15 01:35:58 +0900

    #137 [feat]: 이용약관 Button 컴포넌트 구현

---
+++ app/src/main/java/com/byeboo/app/presentation/splash/termsofservice/component/TermsAllButton.kt
@@ -0,0 +1,72 @@
+package com.byeboo.app.presentation.splash.termsofservice.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import com.byeboo.app.R
+import com.byeboo.app.core.designsystem.ui.theme.ByeBooTheme
+import com.byeboo.app.core.util.noRippleClickable
+
+@Composable
+fun TermsAllButton(
+    onClick: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+    isChecked: Boolean = false,
+
+) {
+    val textColor = if (isChecked) ByeBooTheme.colors.gray50 else ByeBooTheme.colors.gray300
+    val backgroundColor = if (isChecked) ByeBooTheme.colors.primary300Alpha20 else ByeBooTheme.colors.whiteAlpha10
+
+    Column(
+        modifier = modifier.padding(vertical = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .background(
+                    color = backgroundColor, shape = RoundedCornerShape(12.dp)
+                )
+                .then(
+                    if (isChecked) Modifier.border(
+                        width = 1.dp, color = ByeBooTheme.colors.primary300
+                    ) else Modifier
+                )
+                .padding(vertical = 18.dp, horizontal = 24.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(R.drawable.ic_background_check),
+                contentDescription = "all agree checkButton",
+                tint = Color.Unspecified,
+                modifier = modifier
+                    .size(24.dp)
+                    .noRippleClickable { onClick(!isChecked) }
+            )
+
+            Spacer(modifier = modifier.width(8.dp))
+
+            Text(
+                text = "전체 동의",
+                style = ByeBooTheme.typography.body3,
+                color = textColor
+            )
+        }
+    }
+}
