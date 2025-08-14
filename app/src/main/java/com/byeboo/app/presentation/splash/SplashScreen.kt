987612d9c334164f27f15ccef58d9a509347d3a8
JAY <wndkdus102@naver.com>
2025-08-15 01:35:41 +0900

    #137 [feat]: kakao login ui

--- app/src/main/java/com/byeboo/app/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/byeboo/app/presentation/splash/SplashScreen.kt
@@ -1,19 +1,39 @@
 package com.byeboo.app.presentation.splash
 
+import android.R.attr.onClick
+import android.R.attr.text
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.byeboo.app.R
+import com.byeboo.app.core.designsystem.ui.theme.ByeBooTheme
+import com.byeboo.app.core.util.noRippleClickable
 import com.byeboo.app.core.util.screenHeightDp
+import com.byeboo.app.core.util.screenWidthDp
 
 @Composable
 fun SplashRoute(
@@ -33,14 +53,16 @@ fun SplashRoute(
     }
 
     SplashScreen(
+        padding = padding,
+        onClick = { },
         modifier = modifier,
-        padding = padding
     )
 }
 
 @Composable
 fun SplashScreen(
     padding: Dp,
+    onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Box(
@@ -52,6 +74,7 @@ fun SplashScreen(
             modifier = Modifier.fillMaxSize(),
             contentScale = ContentScale.Crop
         )
+
         Image(
             painter = painterResource(R.drawable.img_splash_logo),
             contentDescription = null,
@@ -59,5 +82,41 @@ fun SplashScreen(
                 .padding(horizontal = screenHeightDp(76.dp))
                 .padding(top = screenHeightDp(padding + 250.dp))
         )
+
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 24.dp)
+                .padding(bottom = padding),
+        ) {
+
+            Spacer(modifier = modifier.weight(1f))
+
+            Row(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(color = ByeBooTheme.colors.primary300)
+                    .noRippleClickable(onClick = onClick)
+                    .padding(vertical = 16.dp)
+                ,
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_kakao_logo),
+                    contentDescription = "kakao logo"
+                )
+
+                Spacer(modifier = modifier.width(16.dp))
+
+                Text(
+                    text = "Kakao로 시작하기",
+                    style = ByeBooTheme.typography.body2,
+                    color = ByeBooTheme.colors.black
+                )
+            }
+        }
     }
 }
+
+
