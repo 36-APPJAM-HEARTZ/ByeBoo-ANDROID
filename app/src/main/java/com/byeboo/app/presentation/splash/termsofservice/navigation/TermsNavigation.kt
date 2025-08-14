b5b4b929b915a7b898cbc0c203ec55b28e303e58
JAY <wndkdus102@naver.com>
2025-08-15 02:45:28 +0900

    #137 [feat]: navigation 추가

--- app/src/main/java/com/byeboo/app/presentation/splash/termsofservice/navigation/TermsNavigation.kt
+++ app/src/main/java/com/byeboo/app/presentation/splash/termsofservice/navigation/TermsNavigation.kt
@@ -1,12 +1,18 @@
 package com.byeboo.app.presentation.splash.termsofservice.navigation
 
 import androidx.compose.ui.unit.Dp
+import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.byeboo.app.core.navigation.Route
 import com.byeboo.app.presentation.splash.termsofservice.TermsOfServiceRoute
 import kotlinx.serialization.Serializable
 
+
+fun NavController.navigateToTerms(navOptions: NavOptions? = null) {
+    navigate(Terms, navOptions)
+}
 fun NavGraphBuilder.termsGraph(
     padding: Dp
 ) {
