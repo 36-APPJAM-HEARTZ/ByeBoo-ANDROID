a9e0ef2a2d7409f69cb4d15a1e885dbae7e3faa6
sohee <144779368+sohee6989@users.noreply.github.com>
2025-08-20 13:19:38 +0900

    #148 [mod] : 약관화면 Url 부르는 로직 sideeffect로 관리

--- app/src/main/java/com/byeboo/app/presentation/splash/termsofservice/TermsOfServiceViewModel.kt
+++ app/src/main/java/com/byeboo/app/presentation/splash/termsofservice/TermsOfServiceViewModel.kt
@@ -1,11 +1,15 @@
 package com.byeboo.app.presentation.splash.termsofservice
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,7 +18,18 @@ class TermsOfServiceViewModel @Inject constructor() : ViewModel() {
     private val _uiState = MutableStateFlow(TermsOfServiceUiState())
     val uiState: StateFlow<TermsOfServiceUiState> = _uiState.asStateFlow()
 
+    private val _sideEffect = MutableSharedFlow<TermsOfServiceSideEffect>()
+    val sideEffect = _sideEffect.asSharedFlow()
+
     private val ALL_TERMS: Set<TermType> = TermType.entries.toSet()
+
+    fun onTermsLinkClicked(url: String?) {
+        viewModelScope.launch {
+            url?.let {
+                _sideEffect.emit(TermsOfServiceSideEffect.OpenUrl(url))
+            }
+        }
+    }
 
     fun onAllTermsClick() {
         _uiState.update { state ->
