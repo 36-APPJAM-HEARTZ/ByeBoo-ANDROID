82b94957f519580abb778fad2052a412d4ceb4ac
JAY <wndkdus102@naver.com>
2025-08-22 19:18:33 +0900

    #150 [feat]: 닉네임 local 저장 Repository 작성

--- app/src/main/java/com/byeboo/app/data/repositoryimpl/auth/UserRepositoryImpl.kt
+++ app/src/main/java/com/byeboo/app/data/repositoryimpl/auth/UserRepositoryImpl.kt
@@ -64,7 +64,7 @@ class UserRepositoryImpl @Inject constructor(
         userLocalDataSource.clear()
     }
 
-    override suspend fun updateUserNickName(nickname: String) {
-        userLocalDataSource.saveNickname(nickname)
+    override suspend fun updateUserNickName(nickname: String): Result<Unit> {
+         return runCatching { userLocalDataSource.saveNickname(nickname) }
     }
 }
