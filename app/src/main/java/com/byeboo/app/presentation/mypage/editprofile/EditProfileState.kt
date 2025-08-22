d6aa013c0f675894bfcb21b8d518237e2d8aa6b7
JAY <wndkdus102@naver.com>
2025-08-22 17:49:25 +0900

    #150 [feat]: viewmodel 닉네임 수정 로직 구현

--- app/src/main/java/com/byeboo/app/presentation/mypage/editprofile/EditProfileState.kt
+++ app/src/main/java/com/byeboo/app/presentation/mypage/editprofile/EditProfileState.kt
@@ -1,2 +1,23 @@
 package com.byeboo.app.presentation.mypage.editprofile
 
+import com.byeboo.app.domain.model.auth.NicknameValidationResult
+import com.byeboo.app.presentation.auth.userinfo.model.UserInfoValidationState
+
+data class EditProfileState(
+    val nickname: String ="",
+    val nicknameValidation: NicknameValidationResult = NicknameValidationResult.Valid,
+
+)
+
+
+fun NicknameValidationResult.toValidationState(): UserInfoValidationState {
+    return when (this) {
+        is NicknameValidationResult.Valid -> UserInfoValidationState.Valid
+        is NicknameValidationResult.Invalid -> UserInfoValidationState.Invalid
+        is NicknameValidationResult.Empty -> UserInfoValidationState.Empty
+    }
+}
+
+sealed interface EditProfileSideEffect {
+    data class NavigateToMyPage(val nickname: String): EditProfileSideEffect
+}
--- app/src/main/java/com/byeboo/app/presentation/mypage/editprofile/EditProfileViewModel.kt
+++ app/src/main/java/com/byeboo/app/presentation/mypage/editprofile/EditProfileViewModel.kt
@@ -1,10 +1,62 @@
 package com.byeboo.app.presentation.mypage.editprofile
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.byeboo.app.domain.model.auth.NicknameValidator
+import com.byeboo.app.domain.repository.auth.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class EditProfileViewModel @Inject constructor() : ViewModel() {
+class EditProfileViewModel @Inject constructor(
+    val userRepository: UserRepository
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(EditProfileState())
+    val uiState: StateFlow<EditProfileState> = _uiState.asStateFlow()
+
+    private val _sideEffect = MutableSharedFlow<EditProfileSideEffect>()
+    val sideEffect: SharedFlow<EditProfileSideEffect> = _sideEffect
+
+    companion object {
+        private const val MAX_NICKNAME_LENGTH = 5
+    }
+
+
+    init {
+        viewModelScope.launch {
+            userRepository.getNickname().collect { nickname ->
+                _uiState.update { it.copy(nickname = nickname) }
+            }
+        }
+    }
+
+
+    fun updateNickname(input: String) {
+        if (input.length <= MAX_NICKNAME_LENGTH) {
+            _uiState.update {
+                it.copy(
+                    nickname = input,
+                    nicknameValidation = NicknameValidator.validate(input)
+                )
+            }
+        }
+    }
+
+    fun onBackClicked() {
+        val nickname = uiState.value.nickname
+
+        viewModelScope.launch {
+            _sideEffect.emit(EditProfileSideEffect.NavigateToMyPage(nickname))
+        }
+    }
 
 }
