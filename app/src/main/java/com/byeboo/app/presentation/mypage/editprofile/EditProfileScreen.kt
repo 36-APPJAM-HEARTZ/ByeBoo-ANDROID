f7b542c9130727eb39bae25936a51ff4fe4ee657
JAY <wndkdus102@naver.com>
2025-08-22 18:22:23 +0900

    #150 [feat]: Button 추가

--- app/src/main/java/com/byeboo/app/presentation/mypage/editprofile/EditProfileScreen.kt
+++ app/src/main/java/com/byeboo/app/presentation/mypage/editprofile/EditProfileScreen.kt
@@ -13,8 +13,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Modifier
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
@@ -22,14 +22,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.byeboo.app.R
+import com.byeboo.app.core.designsystem.component.button.ByeBooActivationButton
 import com.byeboo.app.core.designsystem.ui.theme.ByeBooTheme
-import com.byeboo.app.presentation.auth.userinfo.UserInfoState
+import com.byeboo.app.domain.model.auth.NicknameValidationResult
 import com.byeboo.app.presentation.auth.userinfo.component.NicknameTextField
-import com.byeboo.app.presentation.auth.userinfo.model.UserInfoValidationState
 
 @Composable
 fun EditProfileRoute(
@@ -54,7 +52,6 @@ fun EditProfileRoute(
         bottomPadding = bottomPadding,
         onNicknameChange= viewModel::updateNickname,
         onBackClick = viewModel::onBackClicked
-
     )
 }
 
@@ -66,6 +63,8 @@ private fun EditProfileScreen(
     onBackClick: () -> Unit,
     modifier: Modifier = Modifier
 ){
+    val isNicknameValid = uiState.nicknameValidation == NicknameValidationResult.Valid
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -111,6 +110,17 @@ private fun EditProfileScreen(
             validationState = uiState.nicknameValidation.toValidationState(),
             onValueChange = onNicknameChange,
             onClearClick = { onNicknameChange("") }
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        ByeBooActivationButton(
+            buttonDisableColor = ByeBooTheme.colors.whiteAlpha10,
+            buttonText = "완료",
+            buttonDisableTextColor = ByeBooTheme.colors.gray300,
+            isEnabled = isNicknameValid,
+            onClick = {}
+
         )
 
     }
