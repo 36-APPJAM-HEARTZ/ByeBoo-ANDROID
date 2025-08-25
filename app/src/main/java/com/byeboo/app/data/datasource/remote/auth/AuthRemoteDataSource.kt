d2c42eee31c7ad4ef5be8f929e62fb55f3364c5e
JAY <wndkdus102@naver.com>
2025-08-26 02:58:26 +0900

    #145 [feat]: 회원가입, 로그인 API 연결

---
+++ app/src/main/java/com/byeboo/app/data/datasource/remote/auth/AuthRemoteDataSource.kt
@@ -0,0 +1,14 @@
+package com.byeboo.app.data.datasource.remote.auth
+
+import com.byeboo.app.data.dto.base.BaseResponse
+import com.byeboo.app.data.dto.response.auth.KakaoLoginResponseDto
+import com.byeboo.app.data.dto.response.auth.TokenReissueResponseDto
+
+interface AuthRemoteDataSource {
+    suspend fun loginWithKakao(
+        token: String,
+        platform: String
+    ): BaseResponse<KakaoLoginResponseDto>
+
+    suspend fun reissueAccessToken(refreshToken: String): BaseResponse<TokenReissueResponseDto>
+}
--- app/src/main/java/com/byeboo/app/presentation/splash/SplashViewModel.kt
+++ app/src/main/java/com/byeboo/app/presentation/splash/SplashViewModel.kt
@@ -2,7 +2,14 @@ package com.byeboo.app.presentation.splash
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.byeboo.app.domain.model.auth.AuthResult
+import com.byeboo.app.domain.repository.auth.AuthRepository
+import com.byeboo.app.domain.repository.auth.TokenRepository
 import com.byeboo.app.domain.repository.auth.UserRepository
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.AuthError
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.delay
@@ -10,16 +17,67 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 
+
 @HiltViewModel
 class SplashViewModel @Inject constructor(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val authRepository: AuthRepository,
+    private val tokenRepository: TokenRepository
 ) : ViewModel() {
 
-    private val _sideEffect = MutableSharedFlow<SplashState>()
+    private val _sideEffect = MutableSharedFlow<SplashStateSideEffect>()
     val sideEffect = _sideEffect.asSharedFlow()
 
     init {
-        checkUserStatusAndNavigate()
+        //checkUserStatusAndNavigate()
+    }
+
+    fun startKakaoLogin(isLoginAvailable: Boolean) {
+        viewModelScope.launch {
+            if (isLoginAvailable) {
+                _sideEffect.emit(SplashStateSideEffect.StartKakaoTalkLogin)
+            } else {
+                _sideEffect.emit(SplashStateSideEffect.StartKakaoWebLogin)
+            }
+        }
+    }
+
+    fun updateLoginResult(token: OAuthToken?, error: Throwable?) {
+        viewModelScope.launch {
+            when {
+                token != null -> {
+                    kakaoSignIn(token)
+                }
+
+                error is ClientError && error.reason == ClientErrorCause.Cancelled -> {
+                    //Todo: 스낵바
+                }
+
+                error is AuthError -> {
+                    _sideEffect.emit(SplashStateSideEffect.StartKakaoWebLogin)
+                }
+
+                else -> {
+                    // 카카오 로그인 실패
+                }
+            }
+        }
+    }
+
+    private fun kakaoSignIn(token: OAuthToken, platform: String = "KAKAO")
+    {
+        viewModelScope.launch {
+                authRepository.loginWithKakao(
+                    token = token.accessToken,
+                    platform = platform
+                ).onSuccess { authResult ->
+
+                }.onFailure { e ->
+
+                }
+
+        }
+
     }
 
     // TODO: 소셜 로그인 생기면 로직 변경 예정
@@ -30,9 +88,9 @@ class SplashViewModel @Inject constructor(
             val isLoggedIn = userRepository.isLoggedIn()
 
             val effect = if (isLoggedIn) {
-                SplashState.NavigateToHome
+                SplashStateSideEffect.NavigateToHome
             } else {
-                SplashState.NavigateToUserInfo
+                SplashStateSideEffect.NavigateToTermsOfService
             }
 
             _sideEffect.emit(effect)
