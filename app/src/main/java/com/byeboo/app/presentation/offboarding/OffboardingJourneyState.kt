6300900e5b4428bc75ed3b86ea0b03e6cff9e477
sohee <144779368+sohee6989@users.noreply.github.com>
2025-08-25 16:52:29 +0900

    #156 [mod] : 오프보딩 부분 state, ViewModel 한 곳에서 관리하는 방법으로 변경

---
+++ app/src/main/java/com/byeboo/app/presentation/offboarding/OffboardingJourneyState.kt
@@ -0,0 +1,18 @@
+package com.byeboo.app.presentation.offboarding
+
+import com.byeboo.app.presentation.offboarding.model.JourneyCard
+import com.byeboo.app.presentation.offboarding.model.JourneyStatus
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toImmutableList
+
+data class OffboardingJourneyState(
+    val journeyCards: ImmutableList<JourneyCard> = persistentListOf()
+) {
+    val uncompletedCards: ImmutableList<JourneyCard>
+        get() = journeyCards.filter { it.status == JourneyStatus.UNCOMPLETED }.toImmutableList()
+    val completedCards: ImmutableList<JourneyCard>
+        get() = journeyCards.filter { it.status == JourneyStatus.COMPLETED }.toImmutableList()
+    val completedCount: Int get() = completedCards.size
+    val uncompletedCount: Int get() = uncompletedCards.size
+}
---
+++ app/src/main/java/com/byeboo/app/presentation/offboarding/OffboardingJourneyViewModel.kt
@@ -0,0 +1,44 @@
+package com.byeboo.app.presentation.offboarding
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.byeboo.app.domain.repository.offboarding.OffboardingJourneyRepository
+import com.byeboo.app.presentation.offboarding.util.OffboardingJourneyMapper
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class OffboardingJourneyViewModel @Inject constructor(
+    private val offboardingJourneyRepository: OffboardingJourneyRepository,
+    private val mapper: OffboardingJourneyMapper
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(OffboardingJourneyState())
+    val uiState: StateFlow<OffboardingJourneyState> = _uiState.asStateFlow()
+
+    init {
+        getJourneyLists()
+    }
+
+    fun getJourneyLists() {
+        viewModelScope.launch {
+            val result = offboardingJourneyRepository.getOffboardingJourney()
+            result
+                .mapCatching { domain -> mapper.toUiState(domain) }
+                .onSuccess { output ->
+                    _uiState.update {
+                        it.copy(
+                            journeyCards = output.journeyCards
+                        )
+                    }
+            }
+                .onFailure { e ->
+
+                }
+        }
+    }
+}
