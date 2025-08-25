d0abc11e478fa440ddd85d02072634e2f82509b2
sohee <144779368+sohee6989@users.noreply.github.com>
2025-08-25 12:26:19 +0900

    #156 [mod] : 바텀시트 변경

--- app/src/main/java/com/byeboo/app/presentation/quest/component/chip/QuestEmotionChip.kt
+++ app/src/main/java/com/byeboo/app/presentation/quest/component/chip/QuestEmotionChip.kt
@@ -4,21 +4,20 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import com.byeboo.app.core.designsystem.type.LargeTagType
 import com.byeboo.app.core.designsystem.ui.theme.ByeBooTheme
 import com.byeboo.app.core.util.noRippleClickable
-import com.byeboo.app.core.util.screenHeightDp
 import com.byeboo.app.core.util.screenWidthDp
 
 @Composable
@@ -73,11 +72,10 @@ fun EmotionChip(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            painter = painterResource(emotionType.titleIcon),
+            imageVector = ImageVector.vectorResource(id = emotionType.titleIcon),
             contentDescription = null,
             modifier = Modifier
-                .width(screenWidthDp(72.dp))
-                .height(screenHeightDp(56.dp))
+                .padding(horizontal = screenWidthDp(14.dp))
         )
 
         Text(
