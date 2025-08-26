c37f320e77af3a7d69107c612786c9bf90182a76
JAY <wndkdus102@naver.com>
2025-08-26 20:47:24 +0900

    #145 [feat]: key hash 값

--- app/src/main/java/com/byeboo/app/presentation/main/MainActivity.kt
+++ app/src/main/java/com/byeboo/app/presentation/main/MainActivity.kt
@@ -2,17 +2,23 @@ package com.byeboo.app.presentation.main
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.byeboo.app.core.designsystem.ui.theme.ByeBooTheme
+import com.kakao.sdk.common.util.Utility
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val keyHash = Utility.getKeyHash(this)
+        Log.d("KAKAO_KEY_HASH", "keyHash = $keyHash")
+
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
             navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
