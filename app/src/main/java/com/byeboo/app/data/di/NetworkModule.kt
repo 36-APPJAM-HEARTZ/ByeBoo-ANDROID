4939aa0401e41e029baae27bfeba5befb1f5379a
JAY <wndkdus102@naver.com>
2025-08-26 19:33:51 +0900

    #145 [feat]: module 추가

--- app/src/main/java/com/byeboo/app/data/di/NetworkModule.kt
+++ app/src/main/java/com/byeboo/app/data/di/NetworkModule.kt
@@ -1,8 +1,12 @@
 package com.byeboo.app.data.di
 
+
 import com.byeboo.app.BuildConfig
 import com.byeboo.app.core.network.AuthInterceptor
+import com.byeboo.app.core.network.TokenAuthenticator
+import com.byeboo.app.core.network.qualifier.Auth
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.kakao.sdk.auth.AuthApiClient
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -10,6 +14,8 @@ import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
+import okhttp3.Authenticator
+import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -30,16 +36,6 @@ object NetworkModule {
             }
         }
     }
-    @Provides
-    @Singleton
-    fun providesOkHttpClient(
-        authInterceptor: AuthInterceptor,
-        loggingInterceptor: HttpLoggingInterceptor
-    ): OkHttpClient =
-        OkHttpClient.Builder()
-            .addInterceptor(authInterceptor)
-            .addInterceptor(loggingInterceptor)
-            .build()
 
     @OptIn(ExperimentalSerializationApi::class)
     @Provides
@@ -47,6 +43,38 @@ object NetworkModule {
     fun providesConverterFactory(): Converter.Factory = Json.asConverterFactory(
         "application/json".toMediaType()
     )
+
+
+    @Provides
+    @Singleton
+    fun provideAuthInterceptor(authInterceptor: AuthInterceptor) : Interceptor = authInterceptor
+
+    @Provides
+    @Singleton
+    fun provideTokenAuthenticator(authenticator: TokenAuthenticator): Authenticator = authenticator
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(
+        authInterceptor: Interceptor,
+        tokenAuthenticator: TokenAuthenticator,
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor(authInterceptor)
+            .authenticator(tokenAuthenticator)
+            .build()
+
+    @Provides
+    @Singleton
+    @Auth
+    fun providesOkHttpAuthClient(
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
 
     @Provides
     @Singleton
@@ -58,5 +86,18 @@ object NetworkModule {
             .baseUrl(BuildConfig.BASE_URL)
             .client(client)
             .addConverterFactory(converterFactory)
+            .build()
+
+    @Provides
+    @Singleton
+    @Auth
+    fun provideAuthRetrofit(
+        @Auth client: OkHttpClient,
+        factory: Converter.Factory
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(client)
+            .addConverterFactory(factory)
             .build()
 }
