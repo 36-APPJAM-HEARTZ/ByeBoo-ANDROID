296fd7e03f4680ca91a6edc58bbc27eaf99a314d
fredLeeJH <fred54755475@gmail.com>
2025-08-29 01:24:58 +0900

    #159 [mod] : 온보딩 말풍선 크기 조절

--- app/src/main/java/com/byeboo/app/presentation/home/component/HomeSpeechBubbleText.kt
+++ app/src/main/java/com/byeboo/app/presentation/home/component/HomeSpeechBubbleText.kt
@@ -2,9 +2,9 @@ package com.byeboo.app.presentation.home.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -12,6 +12,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
@@ -42,12 +43,12 @@ fun SpeechBubbleWithText(firstText: String, secondText: String, thirdText: Strin
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .wrapContentSize(),
+            .aspectRatio(264f / 62f)
     ) {
         Image(
             imageVector = ImageVector.vectorResource(id = R.drawable.ic_speech_bubble),
             contentDescription = "말풍선",
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.matchParentSize()
         )
         val textToShow = when {
             showFirstText -> firstText
@@ -56,16 +57,20 @@ fun SpeechBubbleWithText(firstText: String, secondText: String, thirdText: Strin
             else -> null
         }
         if (textToShow != null) {
-            Text(
-                text = textToShow,
-                style = ByeBooTheme.typography.body2,
-                color = ByeBooTheme.colors.primary50,
-                textAlign = TextAlign.Center,
+            Box(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 13.5.dp)
-                    .padding(top = 13.2.dp)
-            )
+                    .matchParentSize()
+                    .padding(bottom = 14.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = textToShow,
+                    style = ByeBooTheme.typography.body2,
+                    color = ByeBooTheme.colors.primary50,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
         }
     }
 }
