bf75923e15f16041b50cdf2beab2c2b0d5132d64
JAY <wndkdus102@naver.com>
2025-09-08 17:15:29 +0900

    #183 [mod]: 스낵바 선언 수정

--- app/src/main/java/com/byeboo/app/presentation/mypage/MyPageScreen.kt
+++ app/src/main/java/com/byeboo/app/presentation/mypage/MyPageScreen.kt
@@ -36,6 +36,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.byeboo.app.R
+import com.byeboo.app.core.designsystem.event.LocalSnackBarTrigger
 import com.byeboo.app.core.designsystem.ui.theme.ByeBooTheme
 import com.byeboo.app.core.util.openUrl
 import com.byeboo.app.core.util.screenWidthDp
@@ -53,7 +54,7 @@ fun MyPageRoute(
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     val context = LocalContext.current
-    val lifecycleOwner = LocalLifecycleOwner.current
+    val showSnackBar = LocalSnackBarTrigger.current
 
     if (uiState.showLogoutModal) {
         MyPageModal(
@@ -84,7 +85,7 @@ fun MyPageRoute(
                 is MyPageSideEffect.NavigateToOffboardingCompletedJourney -> navigateToOffboardingCompletedJourney()
                 is MyPageSideEffect.NavigateToTutorial -> navigateToTutorial()
                 is MyPageSideEffect.NavigateToSplash -> navigateToSplash()
-                is MyPageSideEffect.ShowSnackBar -> effect.message
+                is MyPageSideEffect.ShowSnackBar -> showSnackBar(effect.message)
             }
         }
     }
