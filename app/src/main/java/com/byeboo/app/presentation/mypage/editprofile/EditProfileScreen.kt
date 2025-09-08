b9f3d77fbdd992d58af4d709810b25af33e2a678
sohee <144779368+sohee6989@users.noreply.github.com>
2025-09-08 15:20:56 +0900

    #181 [fix] : 뒤로가기 버튼 누를시 텍스트 필드 포커스 해제 시키기

--- app/src/main/java/com/byeboo/app/presentation/mypage/editprofile/EditProfileScreen.kt
+++ app/src/main/java/com/byeboo/app/presentation/mypage/editprofile/EditProfileScreen.kt
@@ -13,11 +13,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.nativeKeyCode
+import androidx.compose.ui.input.key.onPreInterceptKeyBeforeSoftKeyboard
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
@@ -77,8 +82,11 @@ private fun EditProfileScreen(
     onCompleteClick: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val focusManager = LocalFocusManager.current
     val isNicknameValid = uiState.nicknameValidation == NicknameValidationResult.Valid
     val showValidMessage = !uiState.isInitial
+    val isFocused = remember { mutableStateOf(false) }
+
 
     Column(
         modifier = modifier
@@ -126,7 +134,17 @@ private fun EditProfileScreen(
             onValueChange = onNicknameChange,
             onClearClick = onClearClick,
             showValidMessage = showValidMessage,
-            modifier = Modifier.focusRequester(focusRequester)
+            modifier = Modifier
+                .focusRequester(focusRequester)
+                .onPreInterceptKeyBeforeSoftKeyboard { event ->
+                    if (event.key.nativeKeyCode == android.view.KeyEvent.KEYCODE_BACK) {
+                        focusManager.clearFocus(force = true)
+                        isFocused.value = false
+                        true
+                    } else {
+                        false
+                    }
+                }
         )
 
         Spacer(modifier = Modifier.weight(1f))
