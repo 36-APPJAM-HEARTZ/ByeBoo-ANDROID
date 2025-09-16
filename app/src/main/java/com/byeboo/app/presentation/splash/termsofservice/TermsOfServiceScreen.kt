9df8e41258ec761480a93c55fdf2f67493305782
JAY <wndkdus102@naver.com>
2025-09-17 00:02:00 +0900

    #207 [chore]: 안 쓰는 import문 삭제

--- app/src/main/java/com/byeboo/app/presentation/splash/termsofservice/TermsOfServiceScreen.kt
+++ app/src/main/java/com/byeboo/app/presentation/splash/termsofservice/TermsOfServiceScreen.kt
@@ -24,7 +24,6 @@ import com.byeboo.app.R
 import com.byeboo.app.core.designsystem.component.button.ByeBooActivationButton
 import com.byeboo.app.core.designsystem.ui.theme.ByeBooTheme
 import com.byeboo.app.core.util.openUrl
-import com.byeboo.app.presentation.auth.navigation.navigateToUserInfo
 import com.byeboo.app.presentation.splash.termsofservice.component.TermsAllButton
 import com.byeboo.app.presentation.splash.termsofservice.component.TermsCheckButton
 
