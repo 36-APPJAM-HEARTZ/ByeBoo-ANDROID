9c2e70dabe3e98607e092693e95ffe1da82d256f
fredLeeJH <fred54755475@gmail.com>
2025-09-22 11:36:41 +0900

    #206 [feat] : 믹스 패널 트래킹 툴 추가

--- app/src/main/java/com/byeboo/app/ByeBoo.kt
+++ app/src/main/java/com/byeboo/app/ByeBoo.kt
@@ -2,18 +2,25 @@ package com.byeboo.app
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import com.byeboo.app.core.util.MixpanelUtil
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
 class ByeBoo : Application() {
+
+    @Inject
+    lateinit var mixpanelUtil: MixpanelUtil
+
     override fun onCreate() {
         super.onCreate()
 
         initTimber()
         setNightMode()
         initKakaoSdk()
+        initMixpanel()
     }
 
     private fun initTimber() {
@@ -26,5 +33,9 @@ class ByeBoo : Application() {
 
     private fun initKakaoSdk() {
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+    }
+
+    private fun initMixpanel() {
+        mixpanelUtil.initialize(this, BuildConfig.MIXPANEL_TOKEN)
     }
 }
