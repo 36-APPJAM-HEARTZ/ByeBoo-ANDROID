bd3c90c848fb37099ef02f88df0f0a572f45f7d3
znayeonzn <wndkdus102@naver.com>
2025-11-12 18:23:01 +0900

    #222 [feat]: 알림 UI 구현

---
+++ app/src/main/java/com/byeboo/app/presentation/mypage/component/MypageNotification.kt
@@ -0,0 +1,46 @@
+package com.byeboo.app.presentation.mypage.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.byeboo.app.R
+import com.byeboo.app.core.designsystem.ui.theme.ByeBooTheme
+import com.byeboo.app.core.util.noRippleClickable
+
+@Composable
+fun MyPageNotification(
+    modifier: Modifier = Modifier,
+    isEnabledAlarm: Boolean = false,
+    onCheckedClick: (Boolean) -> Unit
+) {
+    val toggle = if (isEnabledAlarm) R.drawable.ic_toggle_on else R.drawable.ic_toggle_off
+    Row(
+        modifier = modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "퀘스트 오픈 알림",
+            style = ByeBooTheme.typography.body3,
+            color = ByeBooTheme.colors.gray50,
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Image(
+            imageVector = ImageVector.vectorResource(toggle),
+            contentDescription = "alarm toggle",
+            modifier = Modifier
+                .noRippleClickable(
+                    onClick = { onCheckedClick(!isEnabledAlarm) }
+                )
+        )
+    }
+}
