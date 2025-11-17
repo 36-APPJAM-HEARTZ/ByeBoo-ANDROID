0494676dcec841aa3d1fbf516704c2672903d7a6
주아연 <142514626+znayeonzn@users.noreply.github.com>
2025-11-17 21:37:24 +0900

    #231 [add]: 필요한 plugin, dependencies 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     alias(libs.plugins.ksp)
     alias(libs.plugins.kotlin.parcelize)
     alias(libs.plugins.ktlint)
+    alias(libs.plugins.googleGmsServices)
 }
 
 val properties = Properties().apply {
--- build.gradle.kts
+++ build.gradle.kts
@@ -9,4 +9,5 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.googleGmsServices) apply false
 }
