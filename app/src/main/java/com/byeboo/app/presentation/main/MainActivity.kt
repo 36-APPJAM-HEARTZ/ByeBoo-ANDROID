cabce2f2f173cffcb75b516cbadccc9e541f5228
주아연 <142514626+znayeonzn@users.noreply.github.com>
2025-11-23 17:50:53 +0900

    #231 [feat]: 푸시 알림 로컬 테스트를 위한 토큰 출력 Log 추가

--- app/src/main/java/com/byeboo/app/presentation/main/MainActivity.kt
+++ app/src/main/java/com/byeboo/app/presentation/main/MainActivity.kt
@@ -2,11 +2,14 @@ package com.byeboo.app.presentation.main
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.byeboo.app.core.designsystem.ui.theme.ByeBooTheme
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,6 +21,19 @@ class MainActivity : ComponentActivity() {
             statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
             navigationBarStyle = SystemBarStyle.dark(Color.TRANSPARENT)
         )
+
+        // token for firebase notification test
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("FCM_TOKEN", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            val token = task.result
+            Log.d("FCM_TOKEN", "Current Token: $token")
+
+        })
+
         setContent {
             ByeBooTheme {
                 MainScreen()
