5f513dd9c559540204a822acc8717cfec765df9e
주아연 <142514626+znayeonzn@users.noreply.github.com>
2025-11-25 00:48:57 +0900

    #239 [feat]: repository 구현

--- app/src/main/java/com/byeboo/app/data/repositoryimpl/fcm/FcmTokenRepositoryImpl.kt
+++ app/src/main/java/com/byeboo/app/data/repositoryimpl/fcm/FcmTokenRepositoryImpl.kt
@@ -1,14 +1,46 @@
 package com.byeboo.app.data.repositoryimpl.fcm
 
 import com.byeboo.app.data.datasource.local.FcmLocalDataSource
+import com.byeboo.app.data.datasource.remote.fcm.FcmRemoteDataSource
+import com.byeboo.app.data.mapper.notification.toData
+import com.byeboo.app.data.mapper.notification.toDomain
+import com.byeboo.app.domain.model.notification.FcmTokenModel
+import com.byeboo.app.domain.model.notification.NotificationSetting
 import com.byeboo.app.domain.repository.fcm.FcmTokenRepository
 import javax.inject.Inject
 
 class FcmTokenRepositoryImpl @Inject constructor(
-    private val fcmLocalDataSource: FcmLocalDataSource
+    private val fcmLocalDataSource: FcmLocalDataSource,
+    private val fcmRemoteDataSource: FcmRemoteDataSource
 ): FcmTokenRepository {
-    override suspend fun saveFcmToken(token: String) {
-        fcmLocalDataSource.saveFcmToken(token)
+    override suspend fun saveFcmToken(fcmToken: FcmTokenModel): Result<Unit> {
+        return runCatching {
+            fcmRemoteDataSource.saveFcmToken(fcmToken.toData())
+            fcmLocalDataSource.saveFcmToken(fcmToken.token)
+
+        }
+    }
+
+    override suspend fun updateFcmToken(fcmToken: FcmTokenModel): Result<Unit> {
+        return runCatching {
+            fcmRemoteDataSource.updateFcmToken(fcmToken.toData())
+            fcmLocalDataSource.saveFcmToken(fcmToken.token)
+        }
+    }
+
+    override suspend fun deleteFcmToken(fcmToken: FcmTokenModel): Result<Unit> {
+        return runCatching {
+            fcmRemoteDataSource.deleteFcmToken(fcmToken.toData())
+            fcmLocalDataSource.deleteFcmToken()
+
+        }
+    }
+
+    override suspend fun allowQuestAlarm(): Result<NotificationSetting> {
+        return runCatching {
+            val response = fcmRemoteDataSource.allowQuestAlarm()
+            response.data.toDomain()
+        }
     }
 
     override suspend fun getFcmToken(): String? {
--- app/src/main/java/com/byeboo/app/domain/repository/fcm/FcmTokenRepository.kt
+++ app/src/main/java/com/byeboo/app/domain/repository/fcm/FcmTokenRepository.kt
@@ -1,6 +1,12 @@
 package com.byeboo.app.domain.repository.fcm
 
+import com.byeboo.app.domain.model.notification.FcmTokenModel
+import com.byeboo.app.domain.model.notification.NotificationSetting
+
 interface FcmTokenRepository {
-    suspend fun saveFcmToken(token: String)
+    suspend fun saveFcmToken(fcmToken: FcmTokenModel): Result<Unit>
+    suspend fun updateFcmToken(fcmToken: FcmTokenModel): Result<Unit>
+    suspend fun deleteFcmToken(fcmToken: FcmTokenModel): Result<Unit>
+    suspend fun allowQuestAlarm(): Result<NotificationSetting>
     suspend fun getFcmToken(): String?
 }
