a448c9e73e64ab6bb61c5ebe72788d1acbed5288
주아연 <142514626+znayeonzn@users.noreply.github.com>
2025-11-25 00:48:30 +0900

    #239 [feat]: 토큰 삭제, 알림 캐싱 local datastore 추가

--- app/src/main/java/com/byeboo/app/data/datasource/local/FcmLocalDataSource.kt
+++ app/src/main/java/com/byeboo/app/data/datasource/local/FcmLocalDataSource.kt
@@ -3,4 +3,7 @@ package com.byeboo.app.data.datasource.local
 interface FcmLocalDataSource {
     suspend fun saveFcmToken(token: String)
     suspend fun getFcmToken(): String?
+    suspend fun deleteFcmToken()
+    suspend fun saveAlarmEnabled(isAlarmEnabled: Boolean)
+    suspend fun isAlarmEnabled(): Boolean
 }
