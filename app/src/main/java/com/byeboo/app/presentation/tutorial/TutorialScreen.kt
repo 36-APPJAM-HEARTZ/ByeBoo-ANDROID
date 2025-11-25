79b3dedbba07a540e509053f98d53e77f1bf0e5b
sohee <144779368+sohee6989@users.noreply.github.com>
2025-11-25 18:38:58 +0900

    #241 [chore]: 튜토리얼 아이콘 변경

--- app/src/main/java/com/byeboo/app/presentation/tutorial/TutorialScreen.kt
+++ app/src/main/java/com/byeboo/app/presentation/tutorial/TutorialScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
@@ -65,11 +66,12 @@ private fun TutorialScreen(
             .padding(top = 67.dp, bottom = bottomPadding)
     ) {
         Icon(
-            imageVector = ImageVector.vectorResource(id = R.drawable.ic_left),
-            contentDescription = "",
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_cancel),
+            contentDescription = null,
             tint = ByeBooTheme.colors.white,
             modifier = Modifier
-                .align(Alignment.Start)
+                .size(24.dp)
+                .align(Alignment.End)
                 .clickable(onClick = onBackClick)
         )
 
