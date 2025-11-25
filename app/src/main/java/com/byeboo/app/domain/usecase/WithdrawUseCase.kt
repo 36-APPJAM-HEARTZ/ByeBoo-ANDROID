92ee551ad57dd8cfee0408725c5a71f6b298e37a
주아연 <142514626+znayeonzn@users.noreply.github.com>
2025-11-25 11:17:46 +0900

    #239 [feat]: 알림 토큰 삭제 api 연결

--- app/src/main/java/com/byeboo/app/domain/usecase/WithdrawUseCase.kt
+++ app/src/main/java/com/byeboo/app/domain/usecase/WithdrawUseCase.kt
@@ -1,24 +1,35 @@
 package com.byeboo.app.domain.usecase
 
+import com.byeboo.app.domain.model.notification.FcmTokenModel
 import com.byeboo.app.domain.repository.auth.AuthRepository
 import com.byeboo.app.domain.repository.auth.TokenRepository
 import com.byeboo.app.domain.repository.auth.UserRepository
+import com.byeboo.app.domain.repository.fcm.FcmTokenRepository
 import javax.inject.Inject
 
 class WithdrawUseCase @Inject constructor(
     private val authRepository: AuthRepository,
     private val tokenRepository: TokenRepository,
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val fcmTokenRepository: FcmTokenRepository
 ) {
     private val accessToken: String
         get() = tokenRepository.getCachedAccessToken()
 
     suspend operator fun invoke(): Result<Unit> {
-        return authRepository.withdrawAccount(accessToken)
-            .onSuccess {
-                tokenRepository.clearTokens()
-                userRepository.clear()
-                tokenRepository.setLoginSplash(true)
-            }
+        val fcmToken = fcmTokenRepository.getFcmToken().orEmpty()
+
+        runCatching {
+            fcmTokenRepository.deleteFcmToken(FcmTokenModel(fcmToken))
+        }
+
+        val withdrawResult = authRepository.withdrawAccount(accessToken)
+
+        withdrawResult.onSuccess {
+            tokenRepository.clearTokens()
+            userRepository.clear()
+            tokenRepository.setLoginSplash(true)
+        }
+        return withdrawResult
     }
 }
