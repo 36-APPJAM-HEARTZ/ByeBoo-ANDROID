f314c32402933c5e7d3dc8cdd9ee753bc7553bd5
sohee <144779368+sohee6989@users.noreply.github.com>
2025-11-26 02:34:28 +0900

    #236 [chore]: import 제거

--- app/src/main/java/com/byeboo/app/presentation/mypage/MyPageScreen.kt
+++ app/src/main/java/com/byeboo/app/presentation/mypage/MyPageScreen.kt
@@ -33,7 +33,6 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
