c811f34fcbc80763a840d739d48f243b0b57ab6b
sohee <144779368+sohee6989@users.noreply.github.com>
2025-11-25 13:55:52 +0900

    #236 [feat]: Context 확장 함수 구현

---
+++ app/src/main/java/com/byeboo/app/core/util/ContextUtil.kt
@@ -0,0 +1,11 @@
+package com.byeboo.app.core.util
+
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+
+fun Context.findActivity(): Activity? = when (this) {
+    is Activity -> this
+    is ContextWrapper -> baseContext.findActivity()
+    else -> null
+}
