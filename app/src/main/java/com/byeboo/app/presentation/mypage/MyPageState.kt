f7b7531309b4d08fe273823d583b423253abe98c
주아연 <142514626+znayeonzn@users.noreply.github.com>
2025-12-02 23:05:47 +0900

    #247 [fix]: 토클 상태 반영 딜레이 오류 수정

--- app/src/main/java/com/byeboo/app/presentation/mypage/MyPageState.kt
+++ app/src/main/java/com/byeboo/app/presentation/mypage/MyPageState.kt
@@ -2,7 +2,7 @@ package com.byeboo.app.presentation.mypage
 
 data class MyPageState(
     val nickname: String = "",
-    val isAlarmEnabled: Boolean = false,
+    val isAlarmEnabled: Boolean? = null,
     val showLogoutModal: Boolean = false,
     val showDeleteAccountModal: Boolean = false,
     val showPermissionModal: Boolean = false
