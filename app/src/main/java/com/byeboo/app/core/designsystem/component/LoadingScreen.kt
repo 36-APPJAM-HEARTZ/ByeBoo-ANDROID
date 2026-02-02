f7cb7a29a5059b4f28ff6efe23410ad92a5a3639
sohee <144779368+sohee6989@users.noreply.github.com>
2026-02-03 04:39:57 +0900

    #259 [feat]: 로딩 화면 생성

---
+++ app/src/main/java/com/byeboo/app/core/designsystem/component/LoadingScreen.kt
@@ -0,0 +1,24 @@
+package com.byeboo.app.core.designsystem.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.byeboo.app.core.designsystem.ui.theme.ByeBooTheme
+
+@Composable
+fun LoadingScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = ByeBooTheme.colors.black),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
