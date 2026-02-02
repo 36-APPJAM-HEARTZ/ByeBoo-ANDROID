5e1e309116e2ddf8fce35e4f446041066672fa8a
sohee <144779368+sohee6989@users.noreply.github.com>
2026-02-03 04:40:49 +0900

    #259 [refactor]: QuestTip 관련 파일들 리팩토링 - 컴포넌트로 분리 & UiState 활용

--- app/src/main/java/com/byeboo/app/presentation/quest/tip/QuestTipViewModel.kt
+++ app/src/main/java/com/byeboo/app/presentation/quest/tip/QuestTipViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
+import com.byeboo.app.core.state.UiState
 import com.byeboo.app.domain.repository.quest.QuestTipRepository
 import com.byeboo.app.presentation.quest.model.Quest
 import com.byeboo.app.presentation.quest.navigation.QuestTip
@@ -26,11 +27,8 @@ class QuestTipViewModel @Inject constructor(
     private val questIdArg = savedStateHandle.toRoute<QuestTip>().questId
     private val questTypeArg = savedStateHandle.toRoute<QuestTip>().questType
 
-    private val _uiState = MutableStateFlow(QuestTipState(
-        questId = questIdArg,
-        questType = questTypeArg
-    ))
-    val uiState: StateFlow<QuestTipState> = _uiState.asStateFlow()
+    private val _uiState = MutableStateFlow<UiState<QuestTipState>>(UiState.Loading)
+    val uiState: StateFlow<UiState<QuestTipState>> = _uiState.asStateFlow()
 
     private val _sideEffect = MutableSharedFlow<QuestTipSideEffect>()
     val sideEffect: SharedFlow<QuestTipSideEffect> = _sideEffect.asSharedFlow()
@@ -44,32 +42,44 @@ class QuestTipViewModel @Inject constructor(
         )
     }
 
-    fun onCloseClicked() {
-        viewModelScope.launch {
-            _sideEffect.emit(QuestTipSideEffect.NavigateToQuest)
-        }
-    }
-
     private fun loadQuestTip(quest: Quest) {
         viewModelScope.launch {
             val result = questTipRepository.getQuestTip(quest.questId)
             result.onSuccess { tip ->
                 _uiState.update {
-                    it.copy(
-                        stepNumber = tip.stepNumber,
-                        questNumber = tip.questNumber,
-                        question = tip.question,
-                        tipAnswer = QuestTipAnswers(
-                            reason = tip.tips.getOrNull(0)?.tipAnswer.orEmpty(),
-                            suggestion = tip.tips.getOrNull(1)?.tipAnswer.orEmpty(),
-                            change = tip.tips.getOrNull(2)?.tipAnswer.orEmpty()
-
+                    UiState.Success(
+                        QuestTipState(
+                            questId = quest.questId,
+                            questType = quest.type,
+                            stepNumber = tip.stepNumber,
+                            questNumber = tip.questNumber,
+                            question = tip.question,
+                            tipAnswer = QuestTipAnswers(
+                                reason = tip.tips.getOrNull(0)?.tipAnswer.orEmpty(),
+                                suggestion = tip.tips.getOrNull(1)?.tipAnswer.orEmpty(),
+                                change = tip.tips.getOrNull(2)?.tipAnswer.orEmpty()
+                            )
                         )
                     )
                 }
             }.onFailure {
+                _uiState.update {
+                    UiState.Success(
+                        QuestTipState(
+                            questId = questIdArg,
+                            questType = questTypeArg
+                        )
+                    )
+                }
+
                 _sideEffect.emit(QuestTipSideEffect.ShowSnackBar("서버에 연결할 수 없습니다. 잠시 후 시도해 주세요."))
             }
+        }
+    }
+
+    fun onCloseClicked() {
+        viewModelScope.launch {
+            _sideEffect.emit(QuestTipSideEffect.NavigateToQuest)
         }
     }
 }
