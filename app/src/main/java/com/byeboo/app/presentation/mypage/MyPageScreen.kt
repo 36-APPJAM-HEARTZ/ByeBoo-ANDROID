8ed1600843a5a7d71243ad56f2daad77a5f52370
sohee <144779368+sohee6989@users.noreply.github.com>
2026-02-04 03:38:23 +0900

    #260 [chore]: 팝업 관련 설정 이전처럼 변경

--- app/src/main/java/com/byeboo/app/presentation/mypage/MyPageScreen.kt
+++ app/src/main/java/com/byeboo/app/presentation/mypage/MyPageScreen.kt
@@ -173,7 +173,7 @@ fun MyPageRoute(
                 }
 
                 is MyPageSideEffect.NavigateToSetting -> {
-                    val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
+                    val intent = Intent(Settings.ACTION_ALL_APPS_NOTIFICATION_SETTINGS).apply {
                         putExtra(Settings.EXTRA_APP_PACKAGE, context.packageName)
                     }
                     context.startActivity(intent)
