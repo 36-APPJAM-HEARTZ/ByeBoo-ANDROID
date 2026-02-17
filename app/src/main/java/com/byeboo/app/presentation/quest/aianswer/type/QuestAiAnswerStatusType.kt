49634d70bc2bc72b6cdc937574e46404cf7255d8
sohee <144779368+sohee6989@users.noreply.github.com>
2026-02-18 01:46:21 +0900

    #269 [feat] ai 답장 상태 관련 타입 구현

---
+++ app/src/main/java/com/byeboo/app/presentation/quest/aianswer/type/QuestAiAnswerStatusType.kt
@@ -0,0 +1,19 @@
+package com.byeboo.app.presentation.quest.aianswer.type
+
+import androidx.annotation.DrawableRes
+import com.byeboo.app.R
+
+enum class QuestAiAnswerStatusType(
+    @DrawableRes val statusImage: Int,
+    val statusContent: String,
+) {
+    LOADING(
+        statusImage = R.drawable.img_ai_bori_looading,
+        statusContent = "보리가 열심히 답변을 작성하고 있어요!"
+    ),
+
+    FAIL(
+        statusImage = R.drawable.img_ai_bori_looading_fail,
+        statusContent = "답변 생성을 실패했어요.\n잠시 뒤에 다시 시도해 주세요."
+    )
+}
