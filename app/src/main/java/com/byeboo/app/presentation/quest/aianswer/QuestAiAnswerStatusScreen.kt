975361ad63831847c2a66f12d85ab024f788754d
sohee <144779368+sohee6989@users.noreply.github.com>
2026-02-18 01:46:49 +0900

    #269 [feat] ai 답장 상태 화면 구현

---
+++ app/src/main/java/com/byeboo/app/presentation/quest/aianswer/QuestAiAnswerStatusScreen.kt
@@ -0,0 +1,90 @@
+package com.byeboo.app.presentation.quest.aianswer
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.byeboo.app.R
+import com.byeboo.app.core.designsystem.ui.theme.ByeBooTheme
+import com.byeboo.app.core.util.screenHeightDp
+import com.byeboo.app.core.util.screenWidthDp
+import com.byeboo.app.presentation.quest.aianswer.type.QuestAiAnswerStatusType
+
+@Composable
+fun QuestAiAnswerStatusScreen(
+    paddingValues: PaddingValues,
+    onCloseClick: () -> Unit,
+    statusType: QuestAiAnswerStatusType,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = ByeBooTheme.colors.black)
+            .padding(
+                top = paddingValues.calculateTopPadding() + screenHeightDp(43.dp),
+                bottom = paddingValues.calculateBottomPadding(),
+            ),
+        verticalArrangement = Arrangement.spacedBy(screenHeightDp(227.dp))
+    ) {
+        Row(
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = screenWidthDp(24.dp)),
+            horizontalArrangement = Arrangement.End,
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_cancel),
+                contentDescription = "back button",
+                tint = ByeBooTheme.colors.white,
+                modifier = modifier.clickable(onClick = onCloseClick),
+            )
+        }
+
+        QuestAiAnswerStatusContent(
+            statusType = statusType
+        )
+    }
+}
+
+@Composable
+private fun QuestAiAnswerStatusContent(
+    statusType: QuestAiAnswerStatusType
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(screenHeightDp(8.dp))
+    ) {
+        Image(
+            painter = painterResource(id = statusType.statusImage),
+            contentDescription = null,
+            modifier = Modifier.size(100.dp)
+        )
+
+        Text(
+            text = statusType.statusContent,
+            color = ByeBooTheme.colors.gray100,
+            style = ByeBooTheme.typography.body3,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
