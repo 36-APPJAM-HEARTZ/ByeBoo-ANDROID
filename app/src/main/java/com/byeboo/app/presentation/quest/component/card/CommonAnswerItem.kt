43697c4f06070832568027b4d86f0992f1ed9031
fredleeJH <fred54755475@gmail.com>
2026-02-20 20:53:06 +0900

    #266 [feat] : 공통 퀘스트 답변 아이템 추가

---
+++ app/src/main/java/com/byeboo/app/presentation/quest/component/card/CommonAnswerItem.kt
@@ -0,0 +1,80 @@
+package com.byeboo.app.presentation.quest.component.card
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.byeboo.app.core.designsystem.ui.theme.ByeBooTheme
+import com.byeboo.app.core.util.screenHeightDp
+import com.byeboo.app.core.util.screenWidthDp
+import com.byeboo.app.presentation.quest.model.CommonAnswerModel
+
+@Composable
+fun CommonAnswerItem(
+    answer: CommonAnswerModel,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(
+                color = ByeBooTheme.colors.whiteAlpha10,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .padding(
+                horizontal = screenWidthDp(24.dp),
+                vertical = screenHeightDp(16.dp)
+            )
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(id = answer.profileIconRes),
+                contentDescription = null,
+                tint = Color.Unspecified
+            )
+
+            Spacer(modifier = Modifier.width(screenWidthDp(4.dp)))
+
+            Text(
+                text = answer.writer,
+                style = ByeBooTheme.typography.body6,
+                color = ByeBooTheme.colors.gray200
+            )
+        }
+
+        Spacer(modifier = Modifier.height(screenHeightDp(12.dp)))
+
+        Text(
+            text = answer.content,
+            style = ByeBooTheme.typography.body3,
+            color = ByeBooTheme.colors.gray100,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+
+        Spacer(modifier = Modifier.height(screenHeightDp(20.dp)))
+
+
+        Text(
+            text = answer.displayTime,
+            style = ByeBooTheme.typography.cap1,
+            color = ByeBooTheme.colors.gray500
+        )
+    }
+}
