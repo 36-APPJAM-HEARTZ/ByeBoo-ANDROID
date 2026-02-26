e5d0ee3d407ad87fd2743df0de7e035f21710356
주아연 <142514626+znayeonzn@users.noreply.github.com>
2026-02-26 18:52:09 +0900

    #268 [feat]: MyAnswer 컴포넌트 구현

---
+++ app/src/main/java/com/byeboo/app/presentation/quest/component/card/MyAnswerItem.kt
@@ -0,0 +1,79 @@
+package com.byeboo.app.presentation.quest.component.card
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.byeboo.app.core.designsystem.ui.theme.ByeBooTheme
+import com.byeboo.app.core.util.noRippleClickable
+import com.byeboo.app.core.util.screenHeightDp
+import com.byeboo.app.core.util.screenWidthDp
+import com.byeboo.app.presentation.quest.model.MyAnswerModel
+
+@Composable
+fun MyAnswerItem(
+    answer: MyAnswerModel,
+    modifier: Modifier = Modifier,
+    onMyAnswerContentClick: (Long) -> Unit = {}
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .noRippleClickable {
+                onMyAnswerContentClick(answer.answerId)
+            }
+            .background(
+                color = ByeBooTheme.colors.whiteAlpha5,
+                shape = RoundedCornerShape(12.dp),
+            )
+            .padding(
+                horizontal = screenWidthDp(24.dp),
+                vertical = screenHeightDp(16.dp),
+            )
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                    text = "Q.",
+                    color = ByeBooTheme.colors.primary200,
+                    style = ByeBooTheme.typography.sub2,
+                    modifier = Modifier.padding(end = screenWidthDp(4.dp)),
+                )
+
+            Text(
+                    text = answer.question,
+                    color = ByeBooTheme.colors.gray100,
+                    style = ByeBooTheme.typography.sub3,
+                )
+        }
+
+        Spacer(modifier = Modifier.padding(screenHeightDp(12.dp)))
+
+        Text(
+            text = answer.content,
+            color = ByeBooTheme.colors.gray100,
+            style = ByeBooTheme.typography.body3,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+
+        Spacer(modifier = Modifier.height(screenHeightDp(20.dp)))
+
+        Text(
+            text = answer.writtenAt,
+            color = ByeBooTheme.colors.gray400,
+            style = ByeBooTheme.typography.cap2,
+        )
+    }
+}
