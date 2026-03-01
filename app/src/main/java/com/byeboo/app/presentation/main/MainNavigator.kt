6be15c55ac489eb8c0d652fa6cf632b95a6dfa21
sohee <144779368+sohee6989@users.noreply.github.com>
2026-03-01 12:50:00 +0900

    #278 [chore]: ktlint 체크

--- app/src/main/java/com/byeboo/app/presentation/main/MainNavigator.kt
+++ app/src/main/java/com/byeboo/app/presentation/main/MainNavigator.kt
@@ -26,8 +26,8 @@ import com.byeboo.app.presentation.offboarding.navigation.navigateToOffboardingQ
 import com.byeboo.app.presentation.offboarding.navigation.navigateToOffboardingQuestReview
 import com.byeboo.app.presentation.quest.behavior.navigation.navigateToQuestBehavior
 import com.byeboo.app.presentation.quest.behavior.navigation.navigateToQuestBehaviorComplete
-import com.byeboo.app.presentation.quest.common.navigation.navigateToQuestCommonWriting
 import com.byeboo.app.presentation.quest.common.navigation.navigateToQuestCommonComplete
+import com.byeboo.app.presentation.quest.common.navigation.navigateToQuestCommonWriting
 import com.byeboo.app.presentation.quest.navigation.Quest
 import com.byeboo.app.presentation.quest.navigation.navigateToQuest
 import com.byeboo.app.presentation.quest.navigation.navigateToQuestCommonAnswer
