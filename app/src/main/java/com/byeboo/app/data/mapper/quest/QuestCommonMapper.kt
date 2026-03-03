50749d655b26426d76526424874acaae91f78c71
주아연 <142514626+znayeonzn@users.noreply.github.com>
2026-03-04 01:33:41 +0900

    #285 [feat] : 나의 공통 퀘스트 답변 mapper 추가

--- app/src/main/java/com/byeboo/app/data/mapper/quest/QuestCommonMapper.kt
+++ app/src/main/java/com/byeboo/app/data/mapper/quest/QuestCommonMapper.kt
@@ -1,9 +1,26 @@
 package com.byeboo.app.data.mapper.quest
 
 import com.byeboo.app.data.dto.request.quest.QuestCommonRequestDto
+import com.byeboo.app.data.dto.response.quest.QuestMyCommonAnswerDto
+import com.byeboo.app.domain.model.quest.QuestAnswerModel
 import com.byeboo.app.domain.model.quest.QuestCommonAnswerRequestModel
+import com.byeboo.app.domain.model.quest.QuestCommonMyAnswerModel
 
 fun QuestCommonAnswerRequestModel.toData(): QuestCommonRequestDto =
     QuestCommonRequestDto(
         answer = this.answer
+    )
+
+fun QuestMyCommonAnswerDto.toDomain(): QuestCommonMyAnswerModel =
+    QuestCommonMyAnswerModel(
+        hasNext = this.hasNext,
+        nextCursor = this.nextCursor,
+        answers = this.answers.map {
+            QuestAnswerModel(
+                question = it.question,
+                answerId = it.answerId,
+                writtenAt = it.writtenAt.split("T")[0],
+                content = it.content
+            )
+        }
     )
