39180dba0c577d2cf767dadccf826ee453f83c04
sohee <144779368+sohee6989@users.noreply.github.com>
2026-03-04 03:17:21 +0900

    #286 [feat]: 사용자 차단 해제 - Service 구현

--- app/src/main/java/com/byeboo/app/data/service/mypage/BlockedUsersService.kt
+++ app/src/main/java/com/byeboo/app/data/service/mypage/BlockedUsersService.kt
@@ -2,9 +2,16 @@ package com.byeboo.app.data.service.mypage
 
 import com.byeboo.app.data.dto.base.NullableBaseResponse
 import com.byeboo.app.data.dto.response.mypage.BlockedUsersResponseDto
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface BlockedUsersService {
     @GET("/api/v1/blocks")
     suspend fun getBlockedUsers(): NullableBaseResponse<BlockedUsersResponseDto>
+
+    @DELETE("/api/v1/blocks/{blockId}")
+    suspend fun unblockUser(
+        @Path("blockId") blockId: Long,
+    ): NullableBaseResponse<Unit>
 }
