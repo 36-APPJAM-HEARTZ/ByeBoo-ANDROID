92959f3e11f4e7bdc0e45beb43cb6cf2ff1d80db
fredleeJH <fred54755475@gmail.com>
2026-03-09 11:04:50 +0900

    #291 [feat] : 코드리뷰 반영

--- app/src/main/java/com/byeboo/app/core/util/ErrorParser.kt
+++ app/src/main/java/com/byeboo/app/core/util/ErrorParser.kt
@@ -1,16 +1,22 @@
 package com.byeboo.app.core.util
 
+import org.json.JSONObject
+import retrofit2.HttpException
+
 object ErrorParser {
-    fun getErrorMessage(throwable: Throwable): String =
-        if (throwable is retrofit2.HttpException) {
-            try {
-                val errorBody = throwable.response()?.errorBody()?.string()
-                val regex = """"message"\s*:\s*"([^"]+)"""".toRegex()
-                regex.find(errorBody ?: "")?.groupValues?.get(1) ?: throwable.message()
-            } catch (e: Exception) {
-                "알 수 없는 서버 에러가 발생했습니다."
-            }
-        } else {
-            throwable.message ?: "네트워크 연결을 확인해 주세요."
+    fun getErrorMessage(throwable: Throwable): String {
+        if (throwable !is HttpException) {
+            return throwable.message ?: "네트워크 연결을 확인해 주세요."
         }
+
+        return runCatching {
+            val errorBody =
+                throwable
+                    .response()
+                    ?.errorBody()
+                    ?.string()
+                    .orEmpty()
+            JSONObject(errorBody).optString("message")
+        }.getOrNull()?.takeIf { it.isNotBlank() } ?: "알 수 없는 서버 에러가 발생했습니다."
+    }
 }
