e09ca97b697db4aff5c8e7e543905afea3ac8875
주아연 <142514626+znayeonzn@users.noreply.github.com>
2026-03-12 23:33:00 +0900

    #296 [chore] : ktlint format

--- app/src/main/java/com/byeboo/app/presentation/quest/component/text/textfield/QuestTextField.kt
+++ app/src/main/java/com/byeboo/app/presentation/quest/component/text/textfield/QuestTextField.kt
@@ -38,7 +38,7 @@ fun QuestTextField(
     val keyboardController = LocalSoftwareKeyboardController.current
     val focusManager = LocalFocusManager.current
 
-    LaunchedEffect(isFocused.value,lineCount.intValue) {
+    LaunchedEffect(isFocused.value, lineCount.intValue) {
         if (isFocused.value) {
             scrollState.animateScrollTo(scrollState.maxValue)
         }
