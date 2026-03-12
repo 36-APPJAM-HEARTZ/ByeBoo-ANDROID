384dd145e612320b876f53bf36c28d606ae4ea42
sohee <144779368+sohee6989@users.noreply.github.com>
2026-03-12 23:00:46 +0900

    #296 [chore]: 타로카드 문구 글자 크기 고정으로 변경

--- app/src/main/java/com/byeboo/app/presentation/home/component/HomeAmuletCard.kt
+++ app/src/main/java/com/byeboo/app/presentation/home/component/HomeAmuletCard.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -21,6 +20,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.byeboo.app.core.designsystem.ui.theme.ByeBooTheme
+import com.byeboo.app.core.util.FixedFontScaleText
 import com.byeboo.app.core.util.screenHeightDp
 import com.byeboo.app.core.util.screenWidthDp
 
@@ -67,7 +67,7 @@ fun HomeAmuletCard(
                     contentScale = ContentScale.Fit,
                 )
 
-                Text(
+                FixedFontScaleText(
                     text = description,
                     style = ByeBooTheme.typography.body6,
                     color = ByeBooTheme.colors.secondary50,
