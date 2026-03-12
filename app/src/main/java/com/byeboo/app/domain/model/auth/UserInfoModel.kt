21f7f660c913c110ded5dab7b277e19e3fc2eefc
sohee <144779368+sohee6989@users.noreply.github.com>
2026-03-12 22:20:35 +0900

    #296 [chore]: 안 쓰는 코드 삭제

--- app/src/main/java/com/byeboo/app/domain/model/auth/UserInfoModel.kt
+++ app/src/main/java/com/byeboo/app/domain/model/auth/UserInfoModel.kt
@@ -9,16 +9,3 @@ data class UserJourney(
     val journey: String,
     val description: String,
 )
-
-// enum class OnboardingQuestStyle(
-//    val displayText: String,
-// ) {
-//    RECORDING("이별 극복"),
-//    REUNION("재회 준비"),
-// }
-//
-// fun OnboardingQuestStyle.toJourneyText(): String =
-//    when (this) {
-//        OnboardingQuestStyle.RECORDING -> "이별 극복"
-//        OnboardingQuestStyle.REUNION -> "재회 준비"
-//    }
