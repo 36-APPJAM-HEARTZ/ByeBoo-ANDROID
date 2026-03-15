3a4e89b860a14300ff23bc1416db67b5962711ad
주아연 <142514626+znayeonzn@users.noreply.github.com>
2026-03-15 16:11:17 +0900

    #302 [fix] : 네트워크 입출력 스레드 분리

--- app/src/main/java/com/byeboo/app/core/network/TokenAuthenticator.kt
+++ app/src/main/java/com/byeboo/app/core/network/TokenAuthenticator.kt
@@ -1,9 +1,13 @@
 package com.byeboo.app.core.network
 
+import com.byeboo.app.core.network.AuthInterceptor.Companion.BEARER
 import com.byeboo.app.domain.repository.auth.AuthRepository
 import com.byeboo.app.domain.repository.auth.TokenRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import okhttp3.Authenticator
 import okhttp3.Request
 import okhttp3.Response
@@ -11,58 +15,87 @@ import okhttp3.Route
 import javax.inject.Inject
 
 class TokenAuthenticator
-    @Inject
-    constructor(
-        private val tokenRepository: TokenRepository,
-        private val authRepository: AuthRepository,
-    ) : Authenticator {
-        override fun authenticate(
-            route: Route?,
-            response: Response,
-        ): Request? =
-            runBlocking {
-                if (responseCount(response) >= 2) return@runBlocking null
+@Inject
+constructor(
+    private val tokenRepository: TokenRepository,
+    private val authRepository: AuthRepository,
+) : Authenticator {
 
-                val refreshToken = tokenRepository.getRefreshToken().firstOrNull()
+    private val mutex = Mutex()
 
-                if (refreshToken.isNullOrEmpty()) {
-                    tokenRepository.clearTokens()
-                    tokenRepository.setLoginSplash(true)
-                    return@runBlocking null
+    override fun authenticate(
+        route: Route?,
+        response: Response,
+    ): Request? {
+        if (responseCount(response) >= 2) return null
+
+        return runBlocking(Dispatchers.IO) {
+            try {
+                mutex.withLock {
+                    val currentAccessToken =
+                        tokenRepository.getAccessToken().firstOrNull().orEmpty()
+                    val requestAccessToken =
+                        response.request.header(AUTHORIZATION)?.substringAfter(BEARER)?.trim()
+                            .orEmpty()
+
+                    if (currentAccessToken != requestAccessToken) {
+                        return@runBlocking response.request
+                            .newBuilder()
+                            .removeHeader(AUTHORIZATION)
+                            .addHeader(AUTHORIZATION, "$BEARER $currentAccessToken")
+                            .build()
+                    }
+
+                    val refreshToken = tokenRepository.getRefreshToken().firstOrNull().orEmpty()
+
+                    if (refreshToken.isEmpty()) {
+                        tokenRepository.clearTokens()
+                        tokenRepository.setLoginSplash(true)
+                        return@runBlocking null
+                    }
+
+                    val result = authRepository.reissueAccessToken(refreshToken)
+                    val newAuthenticatedToken = result.getOrNull()
+
+                    if (newAuthenticatedToken == null) {
+                        tokenRepository.clearTokens()
+                        tokenRepository.setLoginSplash(true)
+                        return@runBlocking null
+                    }
+
+                    tokenRepository.saveTokens(newAuthenticatedToken)
+
+                    return@runBlocking response.request
+                        .newBuilder()
+                        .removeHeader(AUTHORIZATION)
+                        .addHeader(
+                            AUTHORIZATION,
+                            "$BEARER ${newAuthenticatedToken.accessToken}"
+                        )
+                        .build()
                 }
-
-                val result = authRepository.reissueAccessToken(refreshToken)
-
-                val newAuthenticatedToken = result.getOrNull()
-
-                if (newAuthenticatedToken == null) {
-                    tokenRepository.clearTokens()
-                    tokenRepository.setLoginSplash(true)
-                    return@runBlocking null
-                }
-
-                tokenRepository.saveTokens(newAuthenticatedToken)
-
-                response.request
-                    .newBuilder()
-                    .removeHeader(AUTHORIZATION)
-                    .addHeader(AUTHORIZATION, "$BEARER ${newAuthenticatedToken.accessToken}")
-                    .build()
+            } catch (e: Exception) {
+                tokenRepository.clearTokens()
+                tokenRepository.setLoginSplash(true)
+                return@runBlocking null
             }
-
-        private fun responseCount(response: Response): Int {
-            var response: Response? = response
-            var count = 1
-
-            while (response?.priorResponse != null) {
-                count++
-                response = response.priorResponse
-            }
-            return count
         }
 
-        companion object {
-            const val AUTHORIZATION = "Authorization"
-            const val BEARER = "Bearer"
-        }
     }
+
+    private fun responseCount(response: Response): Int {
+        var response: Response? = response
+        var count = 1
+
+        while (response?.priorResponse != null) {
+            count++
+            response = response.priorResponse
+        }
+        return count
+    }
+
+    companion object {
+        const val AUTHORIZATION = "Authorization"
+        const val BEARER = "Bearer"
+    }
+}
