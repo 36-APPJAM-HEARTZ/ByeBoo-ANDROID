0aca475ca629a25d0c89c9c86dabdb348dc5cd6d
주아연 <142514626+znayeonzn@users.noreply.github.com>
2026-05-23 17:15:33 +0900

    #313 [feat] : notification ui

---
+++ app/src/main/java/com/byeboo/app/presentation/notification/NotificationScreen.kt
@@ -0,0 +1,157 @@
+package com.byeboo.app.presentation.notification
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.byeboo.app.R
+import com.byeboo.app.core.designsystem.component.topbar.ByeBooTopbar
+import com.byeboo.app.core.designsystem.ui.theme.ByeBooTheme
+import com.byeboo.app.core.util.noRippleClickable
+import com.byeboo.app.core.util.screenHeightDp
+import com.byeboo.app.core.util.screenWidthDp
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.Alignment
+
+@Composable
+fun NotificationRoute(
+    modifier: Modifier = Modifier,
+    paddingValues: PaddingValues,
+    viewModel: NotificationViewModel = hiltViewModel()
+) {
+
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    NotificationScreen(
+        uiState = uiState,
+        paddingValues = paddingValues,
+        onBackClick = { /*Todo : back action */ },
+        onAllReadClick = {},
+        modifier = modifier
+    )
+}
+
+@Composable
+private fun NotificationScreen(
+    uiState: NotificationUiState,
+    onBackClick: () -> Unit,
+    onAllReadClick: () -> Unit,
+    paddingValues: PaddingValues,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = ByeBooTheme.colors.background)
+            .padding(
+                top = paddingValues.calculateTopPadding() + screenHeightDp(43.dp),
+                bottom = paddingValues.calculateBottomPadding())
+            .padding(horizontal = screenWidthDp(24.dp)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        NotificationTopBar(onBackClick = onBackClick)
+
+        NotificationListHeader(onAllReadClick = onAllReadClick)
+
+        Spacer(modifier = Modifier.height(screenHeightDp(20.dp)))
+
+        NotificationSection(uiState = uiState)
+    }
+}
+
+@Composable
+private fun NotificationTopBar(
+    onBackClick: () -> Unit,
+) {
+    ByeBooTopbar(
+        navigationIcon = {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_left),
+                contentDescription = null,
+                tint = ByeBooTheme.colors.gray50,
+                modifier =
+                    Modifier.noRippleClickable(onClick = onBackClick),
+            )
+        },
+        title = "알림"
+    )
+}
+
+@Composable
+private fun NotificationListHeader(
+    onAllReadClick: () -> Unit,
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+
+    ) {
+        Text(
+            text = "최근 30일까지만 보여요",
+            color = ByeBooTheme.colors.gray300,
+            style = ByeBooTheme.typography.cap1
+        )
+
+        Text(
+            text = "모두 읽기",
+            color = ByeBooTheme.colors.gray300,
+            style = ByeBooTheme.typography.cap1,
+            modifier = Modifier.noRippleClickable(
+                onClick =  {} //Todo : 클릭 시 동작
+            )
+        )
+    }
+}
+
+@Composable
+private fun NotificationSection(
+    uiState: NotificationUiState,
+) {
+    if (uiState.hasNotification) {
+        LazyColumn(
+            contentPadding =PaddingValues(bottom = screenHeightDp(24.dp)),
+            verticalArrangement = Arrangement.spacedBy(screenHeightDp(20.dp)),
+        ) {
+            items(
+                items = uiState.notificationList,
+                key = { it.notificationId },
+            ) { notification ->
+                NotificationCard(
+                    notification = notification,
+                    onClick = { notification.landingLink }
+                )
+            }
+        }
+    } else {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+
+        ) {
+            Text(
+                text = "아직 도착한 알림이 없어요!",
+                color = ByeBooTheme.colors.gray400,
+                style = ByeBooTheme.typography.body6,
+            )
+        }
+    }
+}
