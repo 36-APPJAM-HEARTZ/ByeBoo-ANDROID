6ddcd83ece32062578944efb5e90cc2a15e1d2f4
주아연 <142514626+znayeonzn@users.noreply.github.com>
2026-05-23 17:15:12 +0900

    #313 [feat] : notification dummy 추가

--- app/src/main/java/com/byeboo/app/presentation/notification/NotificationModel.kt
+++ app/src/main/java/com/byeboo/app/presentation/notification/NotificationModel.kt
@@ -16,9 +16,9 @@ data class NotificationModel(
 val dummyNotifications = persistentListOf(
     NotificationModel(
         notificationId = 1L,
-        notificationType  = "QUEST_OPEN", // 아이콘 분기용 타입 (추가하신 스펙 기준)
+        notificationType  = "QUEST_OPEN",
         title = "오늘의 퀘스트 오픈 🌱",
-        content = "새로운 감정 기록 퀘스트가 도착했어요. 내 마음을 들여다볼 시간이에요!",
+        content = "24번째 퀘스트가 오픈됐어요, 시작해볼까요?",
         createdAt = "방금 전",
         isRead = false,
         landingLink = ""
@@ -26,37 +26,47 @@ val dummyNotifications = persistentListOf(
     NotificationModel(
         notificationId = 2L,
         notificationType = "QUEST_EMPATHY",
-        title = "내가 작성한 글에 보리보리쌀님이 공감을 남겼어요",
-        content = "정말 공감되는 글이에요. 오늘 하루도 수고 많으셨습니다!",
+        title = "공통여정에 답변에 공감이 달렸어요",
+        content = "내가 작성한 글에 보리보리쌀님이 공감을 남겼어요",
         createdAt = "1시간 전",
         isRead = false,
-        landingLink = "byeboo://quest/detail/102"
+        landingLink = ""
     ),
     NotificationModel(
         notificationId = 3L,
         notificationType = "QUEST_REPLY",
-        title = "내가 작성한 글에 익명의 유저님이 답변을 남겼어요",
-        content = "저도 비슷한 경험이 있어요. 다들 그렇게 이겨내나 봐요. 응원합니다!",
+        title = "공통여정에 댓글이 달렸어요",
+        content = "내가 작성한 글에 보리보리쌀님이 댓글을 남겼어요",
         createdAt = "3시간 전",
         isRead = true,
-        landingLink = "byeboo://quest/detail/102"
+        landingLink = ""
     ),
     NotificationModel(
         notificationId = 4L,
-        notificationType = "NOTICE",
-        title = "ByeBoo v1.2 업데이트 안내 📣",
-        content = "더욱 따뜻해진 바이부의 새로운 기능들을 만나보세요.",
-        createdAt = "어제",
-        isRead = true,
-        landingLink = "byeboo://notice/20"
+        notificationType = "QUEST_EMPATHY",
+        title = "오늘의 퀘스트 오픈 🌱",
+        content = "25번째 퀘스트가 오픈됐어요, 시작해볼까요?",
+        createdAt = "1시간 전",
+        isRead = false,
+        landingLink = ""
     ),
     NotificationModel(
         notificationId = 5L,
         notificationType = "QUEST_EMPATHY",
-        title = "내가 작성한 글에 감성고양이님이 공감을 남겼어요",
-        content = "당신의 하루를 응원합니다!",
-        createdAt = "2026.05.20",
-        isRead = true,
-        landingLink = "byeboo://quest/detail/88"
-    )
+        title = "오늘의 퀘스트 오픈 🌱",
+        content = "25번째 퀘스트가 오픈됐어요, 시작해볼까요?",
+        createdAt = "1시간 전",
+        isRead = false,
+        landingLink = ""
+    ),
+
+    NotificationModel(
+        notificationId = 6L,
+        notificationType = "QUEST_EMPATHY",
+        title = "오늘의 퀘스트 오픈 🌱",
+        content = "25번째 퀘스트가 오픈됐어요, 시작해볼까요?",
+        createdAt = "1시간 전",
+        isRead = false,
+        landingLink = ""
+    ),
 )
