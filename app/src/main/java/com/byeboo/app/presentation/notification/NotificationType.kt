e16414cb04c0d6e087182cc59413a24763750a90
주아연 <142514626+znayeonzn@users.noreply.github.com>
2026-05-25 17:53:59 +0900

    #313 [feat] : notification type으로 icon 관리

---
+++ app/src/main/java/com/byeboo/app/presentation/notification/NotificationType.kt
@@ -0,0 +1,8 @@
+package com.byeboo.app.presentation.notification
+
+enum class NotificationType {
+    QUEST_OPEN,
+    COMMENT,
+    LIKE,
+    UNKNOWN
+}
--- app/src/main/java/com/byeboo/app/presentation/quest/util/NotificationTypeIcon.kt
+++ app/src/main/java/com/byeboo/app/presentation/quest/util/NotificationTypeIcon.kt
@@ -3,10 +3,11 @@ package com.byeboo.app.presentation.quest.util
 import androidx.annotation.DrawableRes
 import com.byeboo.app.R
 import com.byeboo.app.presentation.notification.NotificationModel
+import com.byeboo.app.presentation.notification.NotificationType
 
 val NotificationModel.iconResId: Int
     @DrawableRes get() = when (this.notificationType) {
-        "QUEST_OPEN" -> R.drawable.ic_notification_quest
-        "COMMENT", "LIKE" -> R.drawable.ic_notification_reaction
-        else -> R.drawable.ic_notification_reaction
+        NotificationType.QUEST_OPEN -> R.drawable.ic_notification_quest
+        NotificationType.COMMENT, NotificationType.LIKE -> R.drawable.ic_notification_reaction
+        NotificationType.UNKNOWN -> R.drawable.ic_notification_reaction
     }
