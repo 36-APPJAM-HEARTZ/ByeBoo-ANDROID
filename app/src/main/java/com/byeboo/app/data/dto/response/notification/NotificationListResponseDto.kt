35f9e4a3eb6cdfbdb2e9645294da2c5332f6933b
주아연 <wndkdus102@naver.com>
2026-06-22 22:52:20 +0900

    #325 [feat] : 알림 목록 조회 api dto 작성

---
+++ app/src/main/java/com/byeboo/app/data/dto/response/notification/NotificationListResponseDto.kt
@@ -0,0 +1,28 @@
+package com.byeboo.app.data.dto.response.notification
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class NotificationListResponseDto(
+    @SerialName("notifications")
+    val notifications: List<NotificationsDto>
+)
+
+@Serializable
+data class NotificationsDto(
+    @SerialName("notificationId")
+    val notificationId: Long,
+    @SerialName("content")
+    val content: String,
+    @SerialName("title")
+    val title: String,
+    @SerialName("isRead")
+    val isRead: Boolean,
+    @SerialName("createdAt")
+    val createdAt: String,
+    @SerialName("landingUrl")
+    val landingUrl: String,
+    @SerialName("notificationType")
+    val notificationType: String
+)
