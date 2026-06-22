06b27e7458f976759b4006f66d83ff3b68e1e95c
주아연 <wndkdus102@naver.com>
2026-06-22 23:33:28 +0900

    #325 [feat] : 홈 화면 알림 존재 여부 api datasource 및 구현체 추가

--- app/src/main/java/com/byeboo/app/data/datasource/remote/notification/NotificationDataSource.kt
+++ app/src/main/java/com/byeboo/app/data/datasource/remote/notification/NotificationDataSource.kt
@@ -2,7 +2,9 @@ package com.byeboo.app.data.datasource.remote.notification
 
 import com.byeboo.app.data.dto.base.BaseResponse
 import com.byeboo.app.data.dto.response.notification.NotificationListResponseDto
+import com.byeboo.app.data.dto.response.notification.NotificationReadStatusResponseDto
 
 interface NotificationDataSource {
+    suspend fun checkHasUnreadNotifications(): BaseResponse<NotificationReadStatusResponseDto>
     suspend fun getNotificationList(): BaseResponse<NotificationListResponseDto>
 }
--- app/src/main/java/com/byeboo/app/data/datasourceimpl/remote/notification/NotificationDataSourceImpl.kt
+++ app/src/main/java/com/byeboo/app/data/datasourceimpl/remote/notification/NotificationDataSourceImpl.kt
@@ -3,12 +3,16 @@ package com.byeboo.app.data.datasourceimpl.remote.notification
 import com.byeboo.app.data.datasource.remote.notification.NotificationDataSource
 import com.byeboo.app.data.dto.base.BaseResponse
 import com.byeboo.app.data.dto.response.notification.NotificationListResponseDto
+import com.byeboo.app.data.dto.response.notification.NotificationReadStatusResponseDto
 import com.byeboo.app.data.service.notification.NotificationService
 import javax.inject.Inject
 
 class NotificationDataSourceImpl @Inject constructor(
     private val notificationService: NotificationService
 ): NotificationDataSource {
+    override suspend fun checkHasUnreadNotifications(): BaseResponse<NotificationReadStatusResponseDto> =
+        notificationService.getReadStatusNotification()
+
     override suspend fun getNotificationList(): BaseResponse<NotificationListResponseDto> =
         notificationService.getNotificationList()
 }
