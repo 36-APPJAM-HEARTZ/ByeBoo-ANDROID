e74187a94883252d8e508c15aef4325a59814ad8
주아연 <wndkdus102@naver.com>
2026-06-22 23:33:40 +0900

    #325 [feat] : 홈 화면 알림 존재 여부 api repository 및 구현체 추가

--- app/src/main/java/com/byeboo/app/data/repositoryimpl/notification/NotificationRepositoryImpl.kt
+++ app/src/main/java/com/byeboo/app/data/repositoryimpl/notification/NotificationRepositoryImpl.kt
@@ -7,17 +7,28 @@ import com.byeboo.app.domain.notification.NotificationRepository
 import javax.inject.Inject
 
 class NotificationRepositoryImpl
-    @Inject constructor(
-        private val notificationDataSource: NotificationDataSource
-    ): NotificationRepository {
-    override suspend fun getNotificationList(): Result<List<Notification>> =
-       runCatching {
-           val response =  notificationDataSource.getNotificationList()
+@Inject constructor(
+    private val notificationDataSource: NotificationDataSource
+) : NotificationRepository {
 
-           if (response.success) {
-               response.data.notifications.map { it.toDomain() }
-           } else {
-               throw Exception(response.message)
-           }
-       }
+    override suspend fun checkHasUnreadNotifications(): Result<Boolean> =
+        runCatching {
+            val response = notificationDataSource.checkHasUnreadNotifications()
+            if (response.success) {
+                response.data.hasUnread
+            } else {
+                throw Exception(response.message)
+            }
+        }
+
+    override suspend fun getNotificationList(): Result<List<Notification>> =
+        runCatching {
+            val response = notificationDataSource.getNotificationList()
+
+            if (response.success) {
+                response.data.notifications.map { it.toDomain() }
+            } else {
+                throw Exception(response.message)
+            }
+        }
 }
--- app/src/main/java/com/byeboo/app/domain/notification/NotificationRepository.kt
+++ app/src/main/java/com/byeboo/app/domain/notification/NotificationRepository.kt
@@ -1,7 +1,9 @@
 package com.byeboo.app.domain.notification
 
+import com.byeboo.app.data.dto.base.BaseResponse
 import com.byeboo.app.domain.model.notification.Notification
 
 interface NotificationRepository {
+    suspend fun checkHasUnreadNotifications(): Result<Boolean>
     suspend fun getNotificationList(): Result<List<Notification>>
 }
