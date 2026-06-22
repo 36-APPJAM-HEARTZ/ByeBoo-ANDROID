3cc70e6951fe26b0470cefecbed76bacbf66d1b6
주아연 <wndkdus102@naver.com>
2026-06-22 22:56:13 +0900

    #325 [feat] : 알림 목록 조회 api 연결

--- app/src/main/java/com/byeboo/app/presentation/notification/NotificationViewModel.kt
+++ app/src/main/java/com/byeboo/app/presentation/notification/NotificationViewModel.kt
@@ -2,25 +2,37 @@ package com.byeboo.app.presentation.notification
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.byeboo.app.domain.notification.NotificationRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class NotificationViewModel
 @Inject
-constructor() : ViewModel() {
+constructor(
+    val notificationRepository: NotificationRepository
+) : ViewModel() {
     private val _uiState = MutableStateFlow(NotificationUiState())
     val uiState: StateFlow<NotificationUiState> = _uiState.asStateFlow()
 
     private val _sideEffect = MutableSharedFlow<NotificationSideEffect>()
     val sideEffect: SharedFlow<NotificationSideEffect> = _sideEffect.asSharedFlow()
+
+    init {
+        viewModelScope.launch {
+            notificationRepository.getNotificationList()
+        }
+    }
+
 
     fun onBackClicked() {
         viewModelScope.launch {
@@ -28,3 +40,5 @@ constructor() : ViewModel() {
         }
     }
 }
+
+
